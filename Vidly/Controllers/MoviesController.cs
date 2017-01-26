5aad4da3b6c48c8582aec8ee0c602025929672a5
Basit Anwer <anwer.basit@gmail.com>
2017-01-26 17:32:42 +0500

    Exc 1: Customer and Movie list with Customer Details

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View(FillMovies());
+        }
+
+        public IEnumerable<Movie> FillMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie() {Id = 1, Name = "Toy Story"},
+                new Movie() {Id = 2, Name = "Finding Nemo"}
+            };
+        }
+    }
+
+}
